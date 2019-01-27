89e1ad3cb96e959f854dcea47067839d3cec9983
bridgs <bridgs.dev@gmail.com>
2019-01-26 20:06:55 -0500

    The game

--- main.lua
+++ main.lua
@@ -1,14 +1,146 @@
+-- Constants
 local GAME_WIDTH = 200
 local GAME_HEIGHT = 200
 local RENDER_SCALE = 3
+local SHOW_BOUNDING_CIRCLES = false
+local TIME_BETWEEN_LASERS = 0.4
+local NUM_STARTING_ASTEROIDS = 4
 
-local ship = {}
-local asteroids = {}
+-- Game objects
+local ship
+local lasers
+local asteroids
+
+-- Images
+local shipImage
+local boostingShipImage
+local massiveAsteroidImage
+local largeAsteroidImage
+local smallAsteroidImage
+
+-- Sound effects
+local spawnSound
+local boostSound
+local destroyedSound
+local laserSound
+local asteroidHitSound
 
 function love.load()
+  -- Load images
+  shipImage = loadImage('img/ship.png')
+  boostingShipImage = loadImage('img/ship-boost.png')
+  massiveAsteroidImage = loadImage('img/asteroid-massive.png')
+  largeAsteroidImage = loadImage('img/asteroid-large.png')
+  smallAsteroidImage = loadImage('img/asteroid-small.png')
+
+  -- Load sound effects
+  spawnSound = love.audio.newSource('sfx/ship-spawn.wav', 'static')
+  boostSound = love.audio.newSource('sfx/ship-boost.wav', 'static')
+  destroyedSound = love.audio.newSource('sfx/ship-destroyed.wav', 'static')
+  laserSound = love.audio.newSource('sfx/laser.wav', 'static')
+  asteroidHitSound = love.audio.newSource('sfx/asteroid-hit.wav', 'static')
+
+  -- Create the player-controlled ship
+  createShip()
+
+  -- Create an empty array for lasers
+  lasers = {}
+
+  -- Create some massive asteroids to start
+  asteroids = {}
+  for i = 1, NUM_STARTING_ASTEROIDS do
+    local vx, vy = createVector(25, math.random(0, 2 * math.pi))
+    createAsteroid(math.random(0, GAME_WIDTH), math.random(0, GAME_HEIGHT), vx, vy, 3)
+  end
 end
 
 function love.update(dt)
+  -- Press the enter key to respawn
+  if ship.isDestroyed and love.keyboard.isDown('return') then
+    createShip()
+  end
+
+  -- Update the ship
+  if not ship.isDestroyed then
+    ship.invulnerabilityTime = math.max(0.00, ship.invulnerabilityTime - dt)
+    ship.boostSoundCooldown = math.max(0.00, ship.boostSoundCooldown - dt)
+    -- Rotate the ship when left/right are pressed
+    if love.keyboard.isDown('left') then
+      ship.rotation = ship.rotation - 6 * dt
+    end
+    if love.keyboard.isDown('right') then
+      ship.rotation = ship.rotation + 6 * dt
+    end
+    -- Accelerate the ship when up is pressed
+    if love.keyboard.isDown('up') then
+      local vx, vy = createVector(150, ship.rotation)
+      ship.vx = ship.vx + vx * dt
+      ship.vy = ship.vy + vy * dt
+      if ship.boostSoundCooldown <= 0.00 then
+        love.audio.play(boostSound:clone())
+        ship.boostSoundCooldown = 0.22
+      end
+    end
+    -- Apply friction to the ship
+    ship.vx = ship.vx * 0.995
+    ship.vy = ship.vy * 0.995
+    -- Move the ship
+    applyMovement(ship, dt)
+    -- Shoot a laser when space is pressed
+    ship.laserCooldown = math.max(0.00, ship.laserCooldown - dt)
+    if love.keyboard.isDown('space') and ship.laserCooldown <= 0 then
+      local vx, vy = createVector(100, ship.rotation)
+      createLaser(ship.x, ship.y, ship.vx + vx, ship.vy + vy)
+      ship.laserCooldown = TIME_BETWEEN_LASERS
+    end
+  end
+
+  -- Update the lasers
+  for i = #lasers, 1, -1 do
+    local laser = lasers[i]
+    applyMovement(laser, dt)
+    -- Lasers disappear after a bit
+    laser.timeUntilDisappear = laser.timeUntilDisappear - dt
+    if laser.timeUntilDisappear < 0.00 then
+      table.remove(lasers, i)
+    end
+  end
+
+  -- Update the asteroids
+  for i = #asteroids, 1, -1 do
+    local asteroid = asteroids[i]
+    applyMovement(asteroid, dt)
+    -- Check for laser + asteroid hits
+    for j = #lasers, 1, -1 do
+      local laser = lasers[j]
+      if objectsAreTouching(laser, asteroid) then
+        table.remove(lasers, j)
+        table.remove(asteroids, i)
+        love.audio.play(asteroidHitSound:clone())
+        -- Spawn smaller asteroids
+        if asteroid.size > 1 then
+          for k = 1, 2 do
+            local vx, vy = createVector(25, math.random(0, 2 * math.pi))
+            createAsteroid(asteroid.x, asteroid.y, vx + asteroid.vx, vy + asteroid.vy, asteroid.size - 1)
+          end
+        end
+        break
+      end
+    end
+    -- Check for ship + asteroid hits
+    if not ship.isDestroyed and ship.invulnerabilityTime <= 0 and objectsAreTouching(ship, asteroid) then
+      ship.isDestroyed = true
+      love.audio.play(destroyedSound:clone())
+    end
+  end
+
+  -- Spawn new asteroids if they all get destroyed
+  if #asteroids == 0 then
+    for i = 1, NUM_STARTING_ASTEROIDS do
+      local vx, vy = createVector(25, math.random(0, 2 * math.pi))
+      createAsteroid(math.random(0, GAME_WIDTH), math.random(0, GAME_HEIGHT), vx, vy, 3)
+    end
+  end
 end
 
 function love.draw()
@@ -16,11 +148,115 @@ function love.draw()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   love.graphics.scale(RENDER_SCALE, RENDER_SCALE)
 
-  -- Make the canvas black
+  -- Black out the screen
   love.graphics.setColor(0, 0, 0, 1)
   love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
-
-  -- Draw the game
   love.graphics.setColor(1, 1, 1, 1)
-  -- ...
+
+  -- Draw the ship
+  if ship.isDestroyed then
+    love.graphics.print('Press enter to respawn', 10, 10)
+  elseif ship.invulnerabilityTime % 0.3 < 0.2 then
+    local image = love.keyboard.isDown('up') and boostingShipImage or shipImage
+    love.graphics.draw(image, ship.x, ship.y, ship.rotation, 1, 1, image:getWidth() / 2, image:getHeight() / 2)
+  end
+
+  -- Draw the lasers
+  for _, laser in ipairs(lasers) do
+    love.graphics.rectangle('fill', laser.x - 1, laser.y - 1, 2, 2)
+  end
+
+  -- Draw the asteroids
+  for _, asteroid in ipairs(asteroids) do
+    local image
+    if asteroid.size == 1 then
+      image = smallAsteroidImage
+    elseif asteroid.size == 2 then
+      image = largeAsteroidImage
+    else
+      image = massiveAsteroidImage
+    end
+    love.graphics.draw(image, asteroid.x, asteroid.y, 0, 1, 1, image:getWidth() / 2, image:getHeight() / 2)
+  end
+
+  -- Draw bounding circles (for debugging)
+  if SHOW_BOUNDING_CIRCLES then
+    love.graphics.setColor(1, 1, 0, 1)
+    if not ship.isDestroyed then
+      love.graphics.circle('line', ship.x, ship.y, ship.radius)
+    end
+    for _, laser in ipairs(lasers) do
+      love.graphics.circle('line', laser.x, laser.y, laser.radius)
+    end
+    for _, asteroid in ipairs(asteroids) do
+      love.graphics.circle('line', asteroid.x, asteroid.y, asteroid.radius)
+    end
+  end
+end
+
+-- Helper methods
+function createShip()
+  ship = {
+    x = GAME_WIDTH / 2,
+    y = GAME_HEIGHT / 2,
+    vx = 0,
+    vy = 0,
+    radius = 4,
+    rotation = 0,
+    laserCooldown = 0.00,
+    isDestroyed = false,
+    invulnerabilityTime = 1.50,
+    boostSoundCooldown = 0.00
+  }
+  love.audio.play(spawnSound:clone())
+end
+
+function createLaser(x, y, vx, vy)
+  table.insert(lasers, {
+    x = x,
+    y = y,
+    vx = vx,
+    vy = vy,
+    radius = 2,
+    timeUntilDisappear = 1.50
+  })
+  love.audio.play(laserSound:clone())
+end
+
+function createAsteroid(x, y, vx, vy, size)
+  table.insert(asteroids, {
+    x = x,
+    y = y,
+    vx = vx,
+    vy = vy,
+    radius = 1 + 3 * size,
+    size = size
+  })
+end
+
+function createVector(magnitude, angle)
+  return magnitude * math.cos(angle), magnitude * math.sin(angle)
+end
+
+-- Returns true if the objects' bounding circles are overlapping
+function objectsAreTouching(obj1, obj2)
+  local dx = obj2.x - obj1.x
+  local dy = obj2.y - obj1.y
+  local distance = math.sqrt(dx * dx + dy * dy)
+  return distance < obj1.radius + obj2.radius
+end
+
+function loadImage(filePath)
+  local image = love.graphics.newImage(filePath)
+  image:setFilter('nearest', 'nearest')
+  return image
+end
+
+function applyMovement(obj, dt)
+  -- Move the game object
+  obj.x = obj.x + obj.vx * dt
+  obj.y = obj.y + obj.vy * dt
+  -- Wrap the game object around the edges of the screen
+  obj.x = (obj.x + GAME_WIDTH) % GAME_WIDTH
+  obj.y = (obj.y + GAME_HEIGHT) % GAME_HEIGHT
 end
